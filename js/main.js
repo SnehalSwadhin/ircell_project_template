9c2875701b4f725d7d8f108f9dc481b54c157280
SnehalSwadhin <54930223+SnehalSwadhin@users.noreply.github.com>
2020-09-20 10:49:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,16 +11,6 @@
     });
 
 /* 2. sticky And Scroll UP */
-    $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-      if (scroll < 400) {
-        $(".header-sticky").removeClass("sticky-bar");
-        $('#back-top').fadeOut(500);
-      } else {
-        $(".header-sticky").addClass("sticky-bar");
-        $('#back-top').fadeIn(500);
-      }
-    });
 
   // Scroll Up
     $('#back-top a').on("click", function () {
@@ -385,3 +375,67 @@ testimonial.slick({
     });
 
 })(jQuery);
+
+(function(){
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.getElementById('site-header');
+  var threshold = 200;
+  var toggled;
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggled = toggleHeader(direction, curScroll);
+    }
+    if(toggled) {
+      prevDirection = direction;
+    }
+    
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    toggled = true;
+    if (direction === 2 && curScroll > threshold) { 
+      
+      //replace 75 with the height of your header in px
+
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
+    }
+    else {
+      toggled = false;
+    }
+    return toggled;
+  };
+  
+  window.addEventListener('scroll', checkScroll);
+
+})();
